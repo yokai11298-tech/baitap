c987d0e061bba30976d24d1326ac97209b6eb930
duong <yokai11298@gmail.com>
2026-09-07 14:37:51 +0700

    Cập nhật bài tập PHP và sửa tên hàm

--- a.php
+++ a.php
@@ -1,3 +1,41 @@
 <?php
+//  1c 2b 3a 4b 5b
 
-echo "Hello from PHP web server!";
+// Bài 1
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . " ";
+    }
+}
+echo "<br><br>"; 
+
+// Bài 2
+function kiemTraNguyenTo($n) {
+    if ($n < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var_dump(kiemTraNguyenTo(17)); 
+echo "<br>";
+var_dump(kiemTraNguyenTo(4));  
+echo "<br><br>";
+
+// Bài 3
+function inHinhChuNhat($chieuRong, $chieuCao) {
+    for ($row = 0; $row < $chieuCao; $row++) {
+        for ($col = 0; $col < $chieuRong; $col++) {
+            echo "* ";
+        }
+        echo "<br>";
+    }
+}
+
+inHinhChuNhat(5, 3);
+?>
